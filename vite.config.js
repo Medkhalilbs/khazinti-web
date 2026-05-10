a78a1e932c4f7e2f25bf1e91b51ec35e4030a838
khalil <medkhalilbs@gmail.com>
2026-05-10 15:49:50 +0100

    chore: update base path to root in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/khazinti-web/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
